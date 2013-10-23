4b0866155b2d3042c6d86f5aa8d6af40720cc0f3
sozdai <alexr@iMac-Alex.local>
2013-10-23 20:17:03 +0300

    FirstClass

---
+++ Lesson1/FirstClass.h
@@ -0,0 +1,13 @@
+//
+//  FirstClass.h
+//  Lesson1
+//
+//  Created by Alex R on 23.10.13.
+//  Copyright (c) 2013 alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstClass : NSObject
+
+@end
---
+++ Lesson1/FirstClass.m
@@ -0,0 +1,13 @@
+//
+//  FirstClass.m
+//  Lesson1
+//
+//  Created by Alex R on 23.10.13.
+//  Copyright (c) 2013 alex. All rights reserved.
+//
+
+#import "FirstClass.h"
+
+@implementation FirstClass
+
+@end
